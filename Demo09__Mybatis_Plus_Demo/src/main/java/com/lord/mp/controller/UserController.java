2a674e8507343c5ccb65644c554a0948827bd0ca
keke0007 <jiangke12@yeah.net>
2019-09-23 20:58:22 +0800

    add ..

---
+++ Demo09__Mybatis_Plus_Demo/src/main/java/com/lord/mp/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.lord.mp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ Author：ke
+ * @ Date： 2019/9/13 17:30
+ * @ Version 1.0
+ */
+@RestController
+@RequestMapping
+public class UserController {
+}
