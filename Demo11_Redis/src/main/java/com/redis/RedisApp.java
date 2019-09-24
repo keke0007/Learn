3e5412dd8ea714906405d21daad94cbb673804c4
kejiang <jiangke12@yeah.net>
2019-09-24 16:55:55 +0800

    add redis.

--- Demo11_Redis/src/main/java/com/redis/RedisApp.java
+++ Demo11_Redis/src/main/java/com/redis/RedisApp.java
@@ -1,9 +1,7 @@
 package com.redis;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author aswl.com
@@ -11,7 +9,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @create 2019/9/24 14:51
  */
 @SpringBootApplication(/*exclude = {DataSourceAutoConfiguration.class}*/)
-@MapperScan("com.redis.mapper")
 public class RedisApp {
     public static void main(String[] args) {
         SpringApplication.run(RedisApp.class);
--- Demo11_Redis/src/main/java/com/redis/config/MybatisPlusConfig.java
+++ Demo11_Redis/src/main/java/com/redis/config/MybatisPlusConfig.java
@@ -1,5 +1,10 @@
 package com.redis.config;
 
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -8,5 +13,38 @@ import org.springframework.context.annotation.Configuration;
  * @create 2019/9/24 15:37
  */
 @Configuration
+@MapperScan("com.redis.mapper")
 public class MybatisPlusConfig {
+    /**
+     * 分页插件
+     *
+     * @return
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+    /**
+     * 性能分析插件
+     *
+     * @return
+     */
+    @Bean
+    public PerformanceInterceptor performanceInterceptor() {
+        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
+        performanceInterceptor.setFormat(true);
+        return performanceInterceptor;
+    }
+
+    /**
+     * 乐观锁插件
+     *
+     * @return
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+
 }
