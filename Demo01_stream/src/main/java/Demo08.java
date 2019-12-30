77921c494d0d0940721b44bcc350c042c918b4a9
kejiang <jiangke12@yeah.net>
2019-12-30 16:45:38 +0800

    java实现websocket整合Stomp协议接受和发送消息

--- Demo01_stream/src/main/java/Demo08.java
+++ Demo01_stream/src/main/java/Demo08.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author jk
@@ -9,7 +8,7 @@ import java.util.List;
  * @create 2019/11/20 9:29
  */
 public class Demo08 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
        /* //调用cmd操作类
         List<String> commend = new ArrayList<>();
         commend.add("ffmpeg -i \"rtsp://admin:12345@192.168.200.22:554/h264/ch1/main/av_stream\" -f flv -r 25 -s 640x480 -ar 44100 \"rtmp://192.168.200.238/oflaDemo/hello10\"");
@@ -21,5 +20,40 @@ public class Demo08 {
         });
 
         System.out.println("AMD,YES!");//可以换成其他提示转换完成的方法*/
+
+        // String a = "A01A01";
+        // String b = "A01A02";
+        // System.out.println(b.indexOf(a));
+
+        // Runtime cmd = Runtime.getRuntime();
+        // Process dir = cmd.exec("cmd /c start dir");
+
+        // List<String> cmd = new ArrayList<>();
+        // cmd.add("cmd /c start dir");
+        // Process start = new ProcessBuilder(Arrays.asList("cmd","/k","start","dir")).start();
+        // String cmd = "cmd /c start dir";
+        String cmd = "cmd /c dir";
+        System.out.println(execCommand(cmd));
     }
+
+    //将cmd执行内容打印到控制台上
+    public static String execCommand(String command) {
+        String line = "";
+        StringBuilder sb = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(
+                        Runtime.getRuntime().exec(command).getInputStream()));) {
+            while ((line = bufferedReader.readLine()) != null)
+                sb.append(line + "\r\n");
+        } catch (IOException e) {
+            return "Invalid command.";
+        }
+
+        return sb.toString();
+    }
+
+//    https://blog.csdn.net/as6757uyy65uy75/article/details/80786593
+//     https://www.jianshu.com/p/43ce90abb5cd
+//     https://www.cnblogs.com/jeryM/p/8459709.html
 }
